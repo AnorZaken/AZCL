17bbdc2725c44e32a34d938c44716a0e66d761b4
AnorZaken <AnorZaken@users.noreply.github.com>
2017-05-09 18:54:05 +0200

    Bits rotation

---
+++ Bits/Rotate.cs
@@ -0,0 +1,269 @@
+﻿
+namespace AZCL.Bits
+{
+    /// <summary>
+    /// Static class for performing bit rotations.
+    /// </summary><remarks>
+    /// A bit rotation is a bit shift with wrap-around. Bits that would get shifted out get shifted back in at the other end. Thus no bits / information is lost.
+    /// </remarks>
+    public static class Rotate
+    {
+        /// <summary>
+        /// Bit rotates <paramref name="value"/> to the left (low towards high).
+        /// </summary><remarks>
+        /// The <paramref name="rotation"/> parameter is anded with 7 (0000'0111) to force it into appropriate range.
+        /// </remarks>
+        /// <param name="value">A value whose bits are to be rotated.</param>
+        /// <param name="rotation">The number of bits to rotate.</param>
+        public static sbyte Left(sbyte value, int rotation)
+        {
+            const int L = 8;
+            const uint V = ~(0xFFFFffff << L);
+            unchecked
+            {
+                rotation &= L - 1;
+                uint val = (uint)value & V;
+                val = val << rotation | val >> L - rotation;
+                return (sbyte)val;
+            }
+        }
+
+        /// <summary>
+        /// Bit rotates <paramref name="value"/> to the left (low towards high).
+        /// </summary><remarks>
+        /// The <paramref name="rotation"/> parameter is anded with 7 (0000'0111) to force it into appropriate range.
+        /// </remarks>
+        /// <param name="value">A value whose bits are to be rotated.</param>
+        /// <param name="rotation">The number of bits to rotate.</param>
+        public static sbyte Left(byte value, int rotation)
+        {
+            const int L = 8;
+            const uint V = ~(0xFFFFffff << L);
+            unchecked
+            {
+                rotation &= L - 1;
+                uint val = (uint)value & V;
+                val = val << rotation | val >> L - rotation;
+                return (sbyte)val;
+            }
+        }
+
+        /// <summary>
+        /// Bit rotates <paramref name="value"/> to the left (low towards high).
+        /// </summary><remarks>
+        /// The <paramref name="rotation"/> parameter is anded with 15 (0000'1111) to force it into appropriate range.
+        /// </remarks>
+        /// <param name="value">A value whose bits are to be rotated.</param>
+        /// <param name="rotation">The number of bits to rotate.</param>
+        public static short Left(short value, int rotation)
+        {
+            const int L = 16;
+            const uint V = ~(0xFFFFffff << L);
+            unchecked
+            {
+                rotation &= L - 1;
+                uint val = (uint)value & V;
+                val = val << rotation | val >> L - rotation;
+                return (short)val;
+            }
+        }
+
+        /// <summary>
+        /// Bit rotates <paramref name="value"/> to the left (low towards high).
+        /// </summary><remarks>
+        /// The <paramref name="rotation"/> parameter is anded with 15 (0000'1111) to force it into appropriate range.
+        /// </remarks>
+        /// <param name="value">A value whose bits are to be rotated.</param>
+        /// <param name="rotation">The number of bits to rotate.</param>
+        public static ushort Left(ushort value, int rotation)
+        {
+            const int L = 16;
+            const uint V = ~(0xFFFFffff << L);
+            unchecked
+            {
+                rotation &= L - 1;
+                uint val = (uint)value & V;
+                val = val << rotation | val >> L - rotation;
+                return (ushort)val;
+            }
+        }
+
+        /// <summary>
+        /// Bit rotates <paramref name="value"/> to the left (low towards high).
+        /// </summary><remarks>
+        /// The <paramref name="rotation"/> parameter is anded with 31 (0001'1111) to force it into appropriate range.
+        /// </remarks>
+        /// <param name="value">A value whose bits are to be rotated.</param>
+        /// <param name="rotation">The number of bits to rotate.</param>
+        public static int Left(int value, int rotation)
+        {
+            const int L = 32;
+            unchecked
+            {
+                return value << rotation | (int)((uint)value >> L - rotation);
+            }
+        }
+
+        /// <summary>
+        /// Bit rotates <paramref name="value"/> to the left (low towards high).
+        /// </summary><remarks>
+        /// The <paramref name="rotation"/> parameter is anded with 31 (0001'1111) to force it into appropriate range.
+        /// </remarks>
+        /// <param name="value">A value whose bits are to be rotated.</param>
+        /// <param name="rotation">The number of bits to rotate.</param>
+        public static uint Left(uint value, int rotation)
+        {
+            const int L = 32;
+            unchecked
+            {
+                return value << rotation | value >> L - rotation;
+            }
+        }
+
+        /// <summary>
+        /// Bit rotates <paramref name="value"/> to the left (low towards high).
+        /// </summary><remarks>
+        /// The <paramref name="rotation"/> parameter is anded with 63 (0011'1111) to force it into appropriate range.
+        /// </remarks>
+        /// <param name="value">A value whose bits are to be rotated.</param>
+        /// <param name="rotation">The number of bits to rotate.</param>
+        public static long Left(long value, int rotation)
+        {
+            const int L = 64;
+            unchecked
+            {
+                return value << rotation | (long)((ulong)value >> L - rotation);
+            }
+        }
+
+        /// <summary>
+        /// Bit rotates <paramref name="value"/> to the left (low towards high).
+        /// </summary><remarks>
+        /// The <paramref name="rotation"/> parameter is anded with 63 (0011'1111) to force it into appropriate range.
+        /// </remarks>
+        /// <param name="value">A value whose bits are to be rotated.</param>
+        /// <param name="rotation">The number of bits to rotate.</param>
+        public static ulong Left(ulong value, int rotation)
+        {
+            const int L = 64;
+            unchecked
+            {
+                return value << rotation | value >> L - rotation;
+            }
+        }
+
+        // -----
+
+        /// <summary>
+        /// Bit rotates <paramref name="value"/> to the right (high towards low).
+        /// </summary><remarks>
+        /// The <paramref name="rotation"/> parameter is anded with 7 (0000'0111) to force it into appropriate range.
+        /// </remarks>
+        /// <param name="value">A value whose bits are to be rotated.</param>
+        /// <param name="rotation">The number of bits to rotate.</param>
+        public static sbyte Right(sbyte value, int rotation)
+        {
+            return Left(value, -rotation);
+        }
+
+        /// <summary>
+        /// Bit rotates <paramref name="value"/> to the right (high towards low).
+        /// </summary><remarks>
+        /// The <paramref name="rotation"/> parameter is anded with 7 (0000'0111) to force it into appropriate range.
+        /// </remarks>
+        /// <param name="value">A value whose bits are to be rotated.</param>
+        /// <param name="rotation">The number of bits to rotate.</param>
+        public static sbyte Right(byte value, int rotation)
+        {
+            return Left(value, -rotation);
+        }
+
+        /// <summary>
+        /// Bit rotates <paramref name="value"/> to the right (high towards low).
+        /// </summary><remarks>
+        /// The <paramref name="rotation"/> parameter is anded with 15 (0000'1111) to force it into appropriate range.
+        /// </remarks>
+        /// <param name="value">A value whose bits are to be rotated.</param>
+        /// <param name="rotation">The number of bits to rotate.</param>
+        public static short Right(short value, int rotation)
+        {
+            return Left(value, -rotation);
+        }
+
+        /// <summary>
+        /// Bit rotates <paramref name="value"/> to the right (high towards low).
+        /// </summary><remarks>
+        /// The <paramref name="rotation"/> parameter is anded with 15 (0000'1111) to force it into appropriate range.
+        /// </remarks>
+        /// <param name="value">A value whose bits are to be rotated.</param>
+        /// <param name="rotation">The number of bits to rotate.</param>
+        public static ushort Right(ushort value, int rotation)
+        {
+            return Left(value, -rotation);
+        }
+
+        /// <summary>
+        /// Bit rotates <paramref name="value"/> to the right (high towards low).
+        /// </summary><remarks>
+        /// The <paramref name="rotation"/> parameter is anded with 31 (0001'1111) to force it into appropriate range.
+        /// </remarks>
+        /// <param name="value">A value whose bits are to be rotated.</param>
+        /// <param name="rotation">The number of bits to rotate.</param>
+        public static int Right(int value, int rotation)
+        {
+            const int L = 32;
+            unchecked
+            {
+                return (int)((uint)value >> rotation) | value << L - rotation;
+            }
+        }
+
+        /// <summary>
+        /// Bit rotates <paramref name="value"/> to the right (high towards low).
+        /// </summary><remarks>
+        /// The <paramref name="rotation"/> parameter is anded with 31 (0001'1111) to force it into appropriate range.
+        /// </remarks>
+        /// <param name="value">A value whose bits are to be rotated.</param>
+        /// <param name="rotation">The number of bits to rotate.</param>
+        public static uint Right(uint value, int rotation)
+        {
+            const int L = 32;
+            unchecked
+            {
+                return value >> rotation | value << L - rotation;
+            }
+        }
+
+        /// <summary>
+        /// Bit rotates <paramref name="value"/> to the right (high towards low).
+        /// </summary><remarks>
+        /// The <paramref name="rotation"/> parameter is anded with 63 (0011'1111) to force it into appropriate range.
+        /// </remarks>
+        /// <param name="value">A value whose bits are to be rotated.</param>
+        /// <param name="rotation">The number of bits to rotate.</param>
+        public static long Right(long value, int rotation)
+        {
+            const int L = 64;
+            unchecked
+            {
+                return (long)((ulong)value >> rotation) | value << L - rotation;
+            }
+        }
+
+        /// <summary>
+        /// Bit rotates <paramref name="value"/> to the right (high towards low).
+        /// </summary><remarks>
+        /// The <paramref name="rotation"/> parameter is anded with 63 (0011'1111) to force it into appropriate range.
+        /// </remarks>
+        /// <param name="value">A value whose bits are to be rotated.</param>
+        /// <param name="rotation">The number of bits to rotate.</param>
+        public static ulong Right(ulong value, int rotation)
+        {
+            const int L = 64;
+            unchecked
+            {
+                return value >> rotation | value << L - rotation;
+            }
+        }
+    }
+}
