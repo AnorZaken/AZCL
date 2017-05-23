535b00ae97496358924efc0ceb0bf2568593a6a6
AnorZaken <AnorZaken@users.noreply.github.com>
2017-05-23 18:49:17 +0200

    Meta.IsAssignableFrom

---
+++ Meta/IsAssignableFrom_T2.cs
@@ -0,0 +1,8 @@
+﻿
+namespace AZCL.Meta
+{
+    internal static class IsAssignableFrom<TFrom, TTo> // TODO: make doc then make public.
+    {
+        public static readonly bool value = typeof(TTo).IsAssignableFrom(typeof(TFrom));
+    }
+}
