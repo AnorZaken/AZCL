f6a064245d0368e59f121831fcb5715498d3ec67
AnorZaken <AnorZaken@users.noreply.github.com>
2017-05-31 10:16:23 +0200

    Java enums - initial commit.

---
+++ Enumeration.cs
@@ -0,0 +1,120 @@
+﻿using AZCL.Collections;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace AZCL
+{
+    public abstract class Enumeration // TODO: doc!
+    {
+        /* This constraint: "where TEnumeration : Enumeration, IEnumerationMarker"; thanks to Enumeration ctor being internal,
+         * Enumeration<T1,T2> requiring T1 to implement IEnumerationMarker, and Enumeration<,> type initialization asserting
+         * direct inheritance; guarantees that TEnumeration will be a user declared type inheriting from Enumeration<,>. */
+        internal static class _Internal<TEnumeration> where TEnumeration : Enumeration, IEnumerationMarker
+        {
+            internal static TEnumeration[] values;
+            internal static FieldInfo[] fields;
+            internal static int initCountDown;
+        }
+
+        public static int Count<TEnumeration>() where TEnumeration : Enumeration, IEnumerationMarker
+        {
+            return _Internal<TEnumeration>.values.Length;
+        }
+
+        public static void Values<TEnumeration>(out TEnumeration[] values) where TEnumeration : Enumeration, IEnumerationMarker
+        {
+            var vsource = _Internal<TEnumeration>.values;
+            var vcopy = new TEnumeration[vsource.Length];
+            Array.Copy(vsource, vcopy, vsource.Length);
+            values = vcopy;
+        }
+
+        public static void Values<TEnumeration>(out ReadOnlyArray<TEnumeration> values) where TEnumeration : Enumeration, IEnumerationMarker
+        {
+            values = _Internal<TEnumeration>.values;
+        }
+
+        public static IEnumerable<TEnumeration> Values<TEnumeration>() where TEnumeration : Enumeration, IEnumerationMarker
+        {
+            var values = _Internal<TEnumeration>.values;
+            for (int i = 0; i < values.Length; ++i)
+                yield return values[i];
+        }
+
+        internal static void Touch<TEnumeration>() where TEnumeration : Enumeration, IEnumerationMarker
+        {
+            var fields = _Internal<TEnumeration>.fields;
+            if (fields != null)
+                foreach (var f in fields)
+                    f.GetValue(null);
+        }
+
+        public static bool IsInitialized<TEnumeration>() where TEnumeration : Enumeration, IEnumerationMarker
+        {
+            return _Internal<TEnumeration>.values != null && _Internal<TEnumeration>.fields == null;
+        }
+
+        public static bool TryInitialize<TEnumeration>() where TEnumeration : Enumeration, IEnumerationMarker
+        {
+            if (IsInitialized<TEnumeration>())
+                return true;
+
+            Touch<TEnumeration>();
+            return IsInitialized<TEnumeration>();
+        }
+
+        /// <summary>
+        /// The ordinal of this (strong) enumeration value.
+        /// </summary><remarks>
+        /// IMPORTANT:<br/>
+        /// The ordinal IS NOT guaranteed to be the same between executions!
+        /// Do not use this value in serialization or any other context where it might persist beyond the execution of the program!
+        /// <br/>
+        /// However at runtime the ordinal IS guaranteed to be fixed, unique among the ordinals of enumerations values of the same type,
+        /// and part of a the zero-based continuous range 0 to (Count - 1).
+        /// (The Count property is static and exists on all types derived from Enumeration.)
+        /// <para/>
+        /// For serialization purposes it is best to use the <see cref="Name"/> property (or simply <see cref="ToString()"/>).
+        /// </remarks>
+        public int Ordinal
+        {
+            get { return ordinal; }
+        }
+
+        /// <summary>
+        /// Name of this enumeration value.
+        /// </summary><remarks>
+        /// Guaranteed to be unique among the names of enumeration values of the same type. (Suitable for serialization.)
+        /// </remarks>
+        public string Name
+        {
+            get { return name; }
+        }
+
+        public sealed override bool Equals(object obj)
+        {
+            return ReferenceEquals(this, obj);
+        }
+
+        public sealed override int GetHashCode()
+        {
+            return ordinal;
+        }
+
+        /// <summary>
+        /// Returns the name of this enumeration value.
+        /// </summary>
+        public sealed override string ToString()
+        {
+            return Name;
+        }
+
+        // Must be internal: This ensures that no external library can inherit directly from this type.
+        internal Enumeration()
+        { }
+
+        internal int ordinal;
+        internal string name;
+    }
+}
---
+++ Enumeration_T2.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Diagnostics;
+using System.Reflection;
+
+namespace AZCL
+{
+    /* An example implementation could look like this:
+
+        public sealed class ExampleEnum : Enumeration<ExampleEnum, ExampleEnum.Const>, IEnumerationMarker
+        {
+            private ExampleEnum()
+		    { }
+
+            public static readonly ExampleEnum Foo = new ExampleEnum();
+            public static readonly ExampleEnum Bar = new ExampleEnum();
+            public static readonly ExampleEnum Bas = new ExampleEnum();
+
+            public enum Const
+            {
+                Foo,
+                Bar,
+                Bas,
+            }
+        }
+    */
+#pragma warning disable CS0660, CS0661
+    public abstract class Enumeration<TStrongEnumeration, TEnum> // TODO: doc!
+        : Enumeration, IEquatable<TStrongEnumeration>, IEquatable<Enumeration<TStrongEnumeration, TEnum>>
+        where TStrongEnumeration : Enumeration<TStrongEnumeration, TEnum>, IEnumerationMarker
+        where TEnum : struct
+    {
+        protected Enumeration()
+        {
+            if (--_Internal<TStrongEnumeration>.initCountDown == 0)
+                __Late__Init((TStrongEnumeration)this);
+        }
+
+        public static int Count
+        {
+            get { return _Internal<TStrongEnumeration>.values.Length; }
+        }
+
+        public TEnum Value
+        {
+            get { return value; }
+        }
+
+        public bool Equals(TStrongEnumeration other)
+        {
+            return ReferenceEquals(this, other);
+        }
+        public bool Equals(Enumeration<TStrongEnumeration, TEnum> other)
+        {
+            return ReferenceEquals(this, other);
+        }
+
+        public static implicit operator Enumeration<TStrongEnumeration, TEnum>(TStrongEnumeration derived)
+        {
+            return derived;
+        }
+
+        public static bool operator ==(Enumeration<TStrongEnumeration, TEnum> left, Enumeration<TStrongEnumeration, TEnum> right)
+        {
+            return ReferenceEquals(left, right);
+        }
+        public static bool operator !=(Enumeration<TStrongEnumeration, TEnum> left, Enumeration<TStrongEnumeration, TEnum> right)
+        {
+            return !ReferenceEquals(left, right);
+        }
+
+        private TEnum value;
+
+#if DEBUG
+        const string // TODO: improve this stuff...
+            THIS = nameof(Enumeration<TStrongEnumeration, TEnum>), // "Enumeration"
+            ERR1 = THIS + ": Generic type-parameter TStrongEnumeration must derive directly from " + THIS + ".",
+            ERR2 = THIS + ": Generic type-parameter TStrongEnumeration must be a sealed class.",
+            ERR3 = THIS + ": Generic type-parameter TEnum must be a System.Enum type.",
+            ERR4 = THIS + ": All TStrongEnumeration constructors must be private.",
+            ERR5 = THIS + ": All public static fields of TStrongEnumeration must be readonly and of type TStrongEnumeration.",
+            ERR6 = THIS + ": All public static fields of TStrongEnumeration must have a unique non-null instance assigned during type initialization.",
+            ERR7 = THIS + ": For each public static TStrongEnumeration field there must exist a TEnum field/value with exactly the same name.";
+#endif
+
+        static Enumeration()
+        {
+#if DEBUG
+            var tStrong = typeof(TStrongEnumeration);
+            Debug.Assert(tStrong.BaseType == typeof(Enumeration<TStrongEnumeration, TEnum>), ERR1);
+            Debug.Assert(tStrong.IsSealed, ERR2);
+            Debug.Assert(typeof(TEnum).IsEnum, ERR3);
+            foreach (var ctor in tStrong.GetConstructors())
+                Debug.Assert(ctor.IsPrivate, ERR4);
+#endif
+            FieldInfo[] fields = typeof(TStrongEnumeration).GetFields(BindingFlags.Static | BindingFlags.Public);
+            _Internal<TStrongEnumeration>.fields = fields;
+            _Internal<TStrongEnumeration>.initCountDown = fields.Length;
+            _Internal<TStrongEnumeration>.values = new TStrongEnumeration[fields.Length];
+        }
+
+        private static void __Late__Init(TStrongEnumeration missingPiece)
+        {
+            var fields = _Internal<TStrongEnumeration>.fields;
+            var values = _Internal<TStrongEnumeration>.values;
+            var tStrong = typeof(TStrongEnumeration);
+            var tWeak = typeof(TEnum);
+
+            for (int i = 0; i < fields.Length; ++i)
+            {
+                var instance = __Field__Init(fields[i], i, ref missingPiece, tStrong, tWeak);
+#if DEBUG
+                Debug.Assert(Array.IndexOf(values, instance, 0, i) == -1, ERR6);
+#endif
+                values[i] = instance;
+            }
+
+            _Internal<TStrongEnumeration>.fields = null;
+        }
+
+        private static TStrongEnumeration __Field__Init(FieldInfo field, int ordinal, ref TStrongEnumeration missingPiece, Type tStrong, Type tEnum)
+        {
+#if DEBUG
+            Debug.Assert(field.IsInitOnly && field.FieldType == tStrong, ERR5);
+#endif
+            var obj = field.GetValue(null);
+            TStrongEnumeration instance;
+            if (obj == null)
+            {
+                instance = missingPiece;
+#if DEBUG
+                Debug.Assert(instance != null, ERR6);
+#endif
+                missingPiece = null;
+            }
+            else
+            {
+                instance = (TStrongEnumeration)obj;
+            }
+            instance.ordinal = ordinal;
+            instance.name = field.Name;
+
+            // TryParse doesn't exist in .net 3.5 :(
+            //bool bParse = Enum.TryParse(instance.name, out instance.value);
+#if DEBUG
+            //Debug.Assert(bParse, ERR7);
+            try
+            {
+                instance.value = (TEnum)Enum.Parse(tEnum, instance.name);
+            }
+            catch
+            {
+                Debug.Assert(false, ERR7);
+            }
+#else
+            instance.value = (TEnum)Enum.Parse(tEnum, instance.name);
+#endif
+            return instance;
+        }
+    }
+#pragma warning restore CS0660, CS0661
+}
