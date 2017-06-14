3024006ccb60f270d84d80b08e128a42fbb23aa1
AnorZaken <AnorZaken@users.noreply.github.com>
2017-06-14 18:27:41 +0200

    Meta IsEnumCompatible

--- Meta/IsEnumCompatible.cs
+++ Meta/IsEnumCompatible.cs
@@ -1,8 +1,17 @@
 ﻿
 namespace AZCL.Meta
 {
-    internal static class IsEnumCompatible<T> // TODO: doc and public
+    /// <summary>
+    /// Static meta class that indicates whether <typeparamref name="T"/> is an enum or enum compatible primitive type.
+    /// </summary>
+    public static class IsEnumCompatible<T>
     {
+        /// <summary>
+        /// Indicates whether <typeparamref name="T"/> is an enum or enum compatible primitive type.
+        /// </summary><remarks>
+        /// The enum compatible types are: bool, char, sbyte, byte, short, ushort, int, uint, long, ulong,
+        /// and of course all user defined enum types (not including the actual System.Enum class!).
+        /// </remarks>
         public static readonly bool value = Evaluate.IsEnumCompatible<T>();
     }
 }
